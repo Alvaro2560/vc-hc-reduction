fb3b920c7e305b769ee957164591c62a865595a0
Alvaro2560 <alvaro.fontenla.33@ull.edu.es>
2024-12-14 23:37:09 +0000

    added k num verification

--- src/components/GraphReduction.tsx
+++ src/components/GraphReduction.tsx
@@ -54,6 +54,11 @@ export default function GraphReduction() {
   }
 
   const performReduction = () => {
+    if (k === 0) {
+      alert('Please set a value for K before performing the reduction.');
+    } else if (k > vcGraph.getAllNodes().length) {
+      alert('K cannot be greater than the number of nodes in the graph.');
+    }
     const handler = new ReductionHandler(vcGraph);
     const reducedHCGraph = handler.performReduction();
     setHCGraph(reducedHCGraph);

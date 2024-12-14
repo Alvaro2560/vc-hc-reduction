1a26cedea319af270221c4f349c615dd8e6869b2
Álvaro Fontenla León <72554062+Alvaro2560@users.noreply.github.com>
2024-12-14 23:15:40 +0000

    Test nodes distance

--- src/components/GraphVisualization.tsx
+++ src/components/GraphVisualization.tsx
@@ -36,8 +36,13 @@ export default function GraphVisualization({ graph, onNodeClick, selectedNodes =
   const graphRef = useRef<any>();
 
   useEffect(() => {
-    graphRef.current.d3Force('charge').strength(-10);
-    graphRef.current.d3Force('link').distance(50);
+    if (graphRef.current && graph.constructor.name === 'VCGraph') {
+      graphRef.current.d3Force('charge').strength(-100);
+      graphRef.current.d3Force('link').distance(50);
+    } else {
+      graphRef.current.d3Force('charge').strength(-1);
+      graphRef.current.d3Force('link').distance(50);
+    }
   }, []);
 
   const nodes = graph.getAllNodes().map((node: any) => ({
